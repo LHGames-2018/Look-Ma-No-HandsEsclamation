0ae35ab2d41997230b51f92cb86d90a8ffdde2b1
William La Berge <will.laberge16@gmail.com>
2018-10-06 13:53:57 -0400

    correction mauvaise identation dijkstra

--- botTools/dijsktra.py
+++ botTools/dijsktra.py
@@ -34,13 +34,13 @@ def dijsktra(graph, initial):
         if min_node is None:
             break
 
-    nodes.remove(min_node)
-    current_weight = visited[min_node]
+        nodes.remove(min_node)
+        current_weight = visited[min_node]
 
-    for edge in graph.edges[min_node]:
-        weight = current_weight + graph.distance[(min_node, edge)]
-        if edge not in visited or weight < visited[edge]:
-            visited[edge] = weight
-            path[edge] = min_node
+        for edge in graph.edges[min_node]:
+            weight = current_weight + graph.distance[(min_node, edge)]
+            if edge not in visited or weight < visited[edge]:
+                visited[edge] = weight
+                path[edge] = min_node
 
     return visited, path
