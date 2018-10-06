662352d7f17a91053a75cfe8f472c83b3490f062
polarbuzzle <fersamw@gmail.com>
2018-10-06 11:53:35 -0400

    collecte de ressource

--- bot/bot.py
+++ bot/bot.py
@@ -20,7 +20,7 @@ class Bot:
         """
 
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        return create_move_action(Point(1, 0))
+        return create_collect_action(Point(1, 0))
 
     def after_turn(self):
         """
